da86254d9ab200311422a92552bc7693996f2c60
tspyrid <tspyrid@gmail.com>
2014-11-23 10:17:44 +0200

    More Changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,10 @@
 ## In this assignment, we see how important is to cache the results, 
-## in oder to use it later, especially for time consuming computations.
-## Matrix inversion usually is very costly,
+## in order to use it later, especially for time consuming computations.
+## Caching is the art of saving on performance by avoiding computation, using memory,
+## and as a data scientist, cashing is very important, useful and in many cases necessary.
+
+## Matrix inversion usually is very costly.
+
 
 ## Function makeCacheMatrix creates a special "matrix" 
 ## object that can cache its inverse.
@@ -22,6 +26,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## cacheSolve function just simply computes the inverse of the "matrix"
 ## returned from the above function called makeCacheMatrix.
+## The main reason that solve is called is that it’s a general 
+## purpose function that you can use to solve matrix equations without 
+## wasting time computing the full inverse, which is often inefficient 
+## and time consuming.
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
